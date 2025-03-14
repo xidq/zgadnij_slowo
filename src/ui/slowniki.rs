c9219412fc6d7254791236498500dd95c7f3011d
Patryk Jerzak <p.jerzu@gmail.com>
2025-03-14 11:01:29 +0100

    added tests

--- src/ui/slowniki.rs
+++ src/ui/slowniki.rs
@@ -10649,13 +10649,40 @@ mod xoxo {
     #[test]
     fn sprawdz_czy_5_sie_zgadza(){
         let mut ilość_błędów:u32 = 0;
-        for bbb in Słowa::pięcioliterowe().słowo[1]{
-            if bbb.len !=5{
-                println!(format!("{} ma {} liter",bbb, bbb.len()));
+        for (bbb,xx) in Słowa::pięcioliterowe().słowo.to_owned(){
+
+            if bbb.len() !=5{
+                println!("{}",format!("{} ma {} liter",bbb, bbb.len()));
                 ilość_błędów += 1;
             }
         }
-        assertEq!(ilość_błędów,0u32);
+        assert_eq!(ilość_błędów,0u32);
         
+    }
+    #[test]
+    fn sprawdz_czy_4_sie_zgadza(){
+        let mut ilość_błędów:u32 = 0;
+        for (bbb,xx) in Słowa::czteroliterowe().słowo.to_owned(){
+
+            if bbb.len() !=4{
+                println!("{}",format!("{} ma {} liter",bbb, bbb.len()));
+                ilość_błędów += 1;
+            }
+        }
+        assert_eq!(ilość_błędów,0u32);
+
+    }
+    #[test]
+    fn sprawdz_czy_3_sie_zgadza(){
+        let mut ilość_błędów:u32 = 0;
+        for (bbb,xx) in Słowa::trzyliterowe().słowo.to_owned(){
+
+            if bbb.len() !=3{
+                println!("{}",format!("{} ma {} liter",bbb, bbb.len()));
+                ilość_błędów += 1;
+            }
+        }
+        assert_eq!(ilość_błędów,0u32);
+
     }
 }
