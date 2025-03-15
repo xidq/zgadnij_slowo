aedfa09a62ac0873985d88c53fad185328755da8
patryk <p.jerzu@gmail.com>
2025-03-15 13:38:17 +0100

    changed style

--- src/ui/reset.rs
+++ src/ui/reset.rs
@@ -5,88 +5,91 @@ use crate::ui::ui_defaults::ZgadnijSlowo;
 pub fn reset_wsio(proxy_self: &mut ZgadnijSlowo){
     proxy_self.wygrałeś = Default::default();
 
-    proxy_self.rząd_0_0 = Default::default();
-    proxy_self.rząd_0_1 = Default::default();
-    proxy_self.rząd_0_2 = Default::default();
-    proxy_self.rząd_0_3 = Default::default();
-    proxy_self.rząd_0_4 = Default::default();
-    proxy_self.rząd_0_5 = Default::default();
+    proxy_self.litery = Default::default();
+    proxy_self.rzędy = Default::default();
 
-    proxy_self.rząd_1_0 = Default::default();
-    proxy_self.rząd_1_1 = Default::default();
-    proxy_self.rząd_1_2 = Default::default();
-    proxy_self.rząd_1_3 = Default::default();
-    proxy_self.rząd_1_4 = Default::default();
-    proxy_self.rząd_1_5 = Default::default();
-
-    proxy_self.rząd_2_0 = Default::default();
-    proxy_self.rząd_2_1 = Default::default();
-    proxy_self.rząd_2_2 = Default::default();
-    proxy_self.rząd_2_3 = Default::default();
-    proxy_self.rząd_2_4 = Default::default();
-    proxy_self.rząd_2_5 = Default::default();
-
-    proxy_self.rząd_3_0 = Default::default();
-    proxy_self.rząd_3_1 = Default::default();
-    proxy_self.rząd_3_2 = Default::default();
-    proxy_self.rząd_3_3 = Default::default();
-    proxy_self.rząd_3_4 = Default::default();
-    proxy_self.rząd_3_5 = Default::default();
-
-    proxy_self.rząd_4_0 = Default::default();
-    proxy_self.rząd_4_1 = Default::default();
-    proxy_self.rząd_4_2 = Default::default();
-    proxy_self.rząd_4_3 = Default::default();
-    proxy_self.rząd_4_4 = Default::default();
-    proxy_self.rząd_4_5 = Default::default();
-
-    proxy_self.rząd_5_0 = Default::default();
-    proxy_self.rząd_5_1 = Default::default();
-    proxy_self.rząd_5_2 = Default::default();
-    proxy_self.rząd_5_3 = Default::default();
-    proxy_self.rząd_5_4 = Default::default();
-    proxy_self.rząd_5_5 = Default::default();
-
-    proxy_self.litera_0_0 = Default::default();
-    proxy_self.litera_0_1 = Default::default();
-    proxy_self.litera_0_2 = Default::default();
-    proxy_self.litera_0_3 = Default::default();
-    proxy_self.litera_0_4 = Default::default();
-    proxy_self.litera_0_5 = Default::default();
-
-    proxy_self.litera_1_0 = Default::default();
-    proxy_self.litera_1_1 = Default::default();
-    proxy_self.litera_1_2 = Default::default();
-    proxy_self.litera_1_3 = Default::default();
-    proxy_self.litera_1_4 = Default::default();
-    proxy_self.litera_1_5 = Default::default();
-
-    proxy_self.litera_2_0 = Default::default();
-    proxy_self.litera_2_1 = Default::default();
-    proxy_self.litera_2_2 = Default::default();
-    proxy_self.litera_2_3 = Default::default();
-    proxy_self.litera_2_4 = Default::default();
-    proxy_self.litera_2_5 = Default::default();
-
-    proxy_self.litera_3_0 = Default::default();
-    proxy_self.litera_3_1 = Default::default();
-    proxy_self.litera_3_2 = Default::default();
-    proxy_self.litera_3_3 = Default::default();
-    proxy_self.litera_3_4 = Default::default();
-    proxy_self.litera_3_5 = Default::default();
-
-    proxy_self.litera_4_0 = Default::default();
-    proxy_self.litera_4_1 = Default::default();
-    proxy_self.litera_4_2 = Default::default();
-    proxy_self.litera_4_3 = Default::default();
-    proxy_self.litera_4_4 = Default::default();
-    proxy_self.litera_4_5 = Default::default();
-
-    proxy_self.litera_5_0 = Default::default();
-    proxy_self.litera_5_1 = Default::default();
-    proxy_self.litera_5_2 = Default::default();
-    proxy_self.litera_5_3 = Default::default();
-    proxy_self.litera_5_4 = Default::default();
-    proxy_self.litera_5_5 = Default::default();
+    // proxy_self.rząd_0_0 = Default::default();
+    // proxy_self.rząd_0_1 = Default::default();
+    // proxy_self.rząd_0_2 = Default::default();
+    // proxy_self.rząd_0_3 = Default::default();
+    // proxy_self.rząd_0_4 = Default::default();
+    // proxy_self.rząd_0_5 = Default::default();
+    // 
+    // proxy_self.rząd_1_0 = Default::default();
+    // proxy_self.rząd_1_1 = Default::default();
+    // proxy_self.rząd_1_2 = Default::default();
+    // proxy_self.rząd_1_3 = Default::default();
+    // proxy_self.rząd_1_4 = Default::default();
+    // proxy_self.rząd_1_5 = Default::default();
+    // 
+    // proxy_self.rząd_2_0 = Default::default();
+    // proxy_self.rząd_2_1 = Default::default();
+    // proxy_self.rząd_2_2 = Default::default();
+    // proxy_self.rząd_2_3 = Default::default();
+    // proxy_self.rząd_2_4 = Default::default();
+    // proxy_self.rząd_2_5 = Default::default();
+    // 
+    // proxy_self.rząd_3_0 = Default::default();
+    // proxy_self.rząd_3_1 = Default::default();
+    // proxy_self.rząd_3_2 = Default::default();
+    // proxy_self.rząd_3_3 = Default::default();
+    // proxy_self.rząd_3_4 = Default::default();
+    // proxy_self.rząd_3_5 = Default::default();
+    // 
+    // proxy_self.rząd_4_0 = Default::default();
+    // proxy_self.rząd_4_1 = Default::default();
+    // proxy_self.rząd_4_2 = Default::default();
+    // proxy_self.rząd_4_3 = Default::default();
+    // proxy_self.rząd_4_4 = Default::default();
+    // proxy_self.rząd_4_5 = Default::default();
+    // 
+    // proxy_self.rząd_5_0 = Default::default();
+    // proxy_self.rząd_5_1 = Default::default();
+    // proxy_self.rząd_5_2 = Default::default();
+    // proxy_self.rząd_5_3 = Default::default();
+    // proxy_self.rząd_5_4 = Default::default();
+    // proxy_self.rząd_5_5 = Default::default();
+    // 
+    // proxy_self.litera_0_0 = Default::default();
+    // proxy_self.litera_0_1 = Default::default();
+    // proxy_self.litera_0_2 = Default::default();
+    // proxy_self.litera_0_3 = Default::default();
+    // proxy_self.litera_0_4 = Default::default();
+    // proxy_self.litera_0_5 = Default::default();
+    // 
+    // proxy_self.litera_1_0 = Default::default();
+    // proxy_self.litera_1_1 = Default::default();
+    // proxy_self.litera_1_2 = Default::default();
+    // proxy_self.litera_1_3 = Default::default();
+    // proxy_self.litera_1_4 = Default::default();
+    // proxy_self.litera_1_5 = Default::default();
+    // 
+    // proxy_self.litera_2_0 = Default::default();
+    // proxy_self.litera_2_1 = Default::default();
+    // proxy_self.litera_2_2 = Default::default();
+    // proxy_self.litera_2_3 = Default::default();
+    // proxy_self.litera_2_4 = Default::default();
+    // proxy_self.litera_2_5 = Default::default();
+    // 
+    // proxy_self.litera_3_0 = Default::default();
+    // proxy_self.litera_3_1 = Default::default();
+    // proxy_self.litera_3_2 = Default::default();
+    // proxy_self.litera_3_3 = Default::default();
+    // proxy_self.litera_3_4 = Default::default();
+    // proxy_self.litera_3_5 = Default::default();
+    // 
+    // proxy_self.litera_4_0 = Default::default();
+    // proxy_self.litera_4_1 = Default::default();
+    // proxy_self.litera_4_2 = Default::default();
+    // proxy_self.litera_4_3 = Default::default();
+    // proxy_self.litera_4_4 = Default::default();
+    // proxy_self.litera_4_5 = Default::default();
+    // 
+    // proxy_self.litera_5_0 = Default::default();
+    // proxy_self.litera_5_1 = Default::default();
+    // proxy_self.litera_5_2 = Default::default();
+    // proxy_self.litera_5_3 = Default::default();
+    // proxy_self.litera_5_4 = Default::default();
+    // proxy_self.litera_5_5 = Default::default();
 
 }
